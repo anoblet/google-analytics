002b463c3dee438bd26ca4a0b5efed9460255cf5
Andrew Noblet <andrewbnoblet@gmail.com>
2019-09-03 13:06:48 -0400

    Chore: Save

--- src/Component.ts
+++ src/Component.ts
@@ -2,30 +2,45 @@ import { LitElement, customElement, html, property } from "lit-element";
 
 @customElement("google-analytics")
 export class GoogleAnalyticsComponent extends LitElement {
-  @property() property: string;
+  @property() account: string;
 
   createRenderRoot() {
     return this;
   }
 
   firstUpdated() {
-    const gtag = document.createElement("script");
-    gtag.src = "https://www.googletagmanager.com/gtag/js?id=UA-63899225-2";
-    this.appendChild(gtag);
-    const script = document.createElement("script");
-    script.innerText = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${this.property}');
-    `;
-    this.appendChild(script);
+    this.install();
+    this.observeLocation();
+  }
 
+  public install() {
+    const scriptOne = document.createElement("script");
+    scriptOne.src = "https://www.googletagmanager.com/gtag/js?id=UA-63899225-2";
+    this.appendChild(scriptOne);
+
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function() {
+      window.dataLayer.push(arguments);
+    };
+    window.gtag("js", new Date());
+    window.gtag("config", "${this.account}");
+
+    // const scriptTwo = document.createElement("script");
+    // scriptTwo.innerText = `
+    //   window.dataLayer = window.dataLayer || [];
+    //   function gtag(){dataLayer.push(arguments);}
+    //   gtag('js', new Date());
+    //   gtag('config', '${this.account}');
+    // `;
+    // this.appendChild(scriptTwo);
+  }
+
+  public observeLocation() {
     var currentPath = window.location.pathname;
     setInterval(() => {
       if (window.location.pathname !== currentPath) {
         currentPath = window.location.pathname;
-        window.gtag("config", this.property, { page_path: currentPath });
+        window.gtag("config", this.account, { page_path: currentPath });
       }
     }, 100);
   }
